e8d82f2a5fd94de893910e830716076716d32ddd
Jears017 <turban01@mail.ru>
2022-03-02 15:15:47 +0300

    change actions planets saga

--- src/sagas/planetsSaga.js
+++ src/sagas/planetsSaga.js
@@ -1,9 +1,10 @@
 import { takeEvery } from "redux-saga/effects";
+import { SAY_HELLO } from "../constants";
 
 function* planetsSagaWorker() {
   yield "Hello";
 }
 
 export function* planetsWorker() {
-  yield takeEvery("SAY_HELLO", planetsSagaWorker);
+  yield takeEvery(SAY_HELLO, planetsSagaWorker);
 }
