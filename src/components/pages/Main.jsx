d6f341c029b4452341ccd4b9dd4ed8ae6145dc6a
Jears017 <turban01@mail.ru>
2022-03-10 13:29:39 +0300

    change main page

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Button } from '@mui/material'
 import { makeStyles } from '@mui/styles'
+import { Link } from 'react-router-dom'
+
+import { PLANETS_PAGE_PATH } from '@/constants'
 
 const useStyles = makeStyles(theme => ({
   mainContainer: {
     display: 'flex',
     justifyContent: 'center',
+  },
+  mainContent: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  mainLink: {
+    textDecoration: 'none',
+    fontSize: 37,
   },
 }))
 
@@ -13,7 +24,16 @@ export default function Main () {
   const classes = useStyles()
   return (
     <Box className={classes.mainContainer}>
-      <Typography variant="h1">Star Wars Application!</Typography>
+      <Box>
+        <Typography variant="h1">Star Wars Application!</Typography>
+        <Box className={classes.mainContent}>
+          <Link className={classes.mainLink} to={PLANETS_PAGE_PATH}>
+            <Button variant="contained" size="large">
+              Get Started
+            </Button>
+          </Link>
+        </Box>
+      </Box>
     </Box>
   )
 }
