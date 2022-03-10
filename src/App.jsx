76689459790257b93a3c6922471ba1ecd9e8f988
Jears017 <turban01@mail.ru>
2022-03-10 11:02:46 +0300

    fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
 
 import { Header } from '@/components/blocks/Header'
-import Characters from './components/pages/Characters'
+import Characters from '@/components/pages/Characters'
+import Planets from '@/components/pages/Planets'
+import { PLANETS_PAGE_PATH, CHARACTERS_PAGE_PATH } from '@/constants'
 
 export default function App () {
   return (
     <Box>
       <Header />
-      <Characters/>
+      <Routes>
+        <Route path={PLANETS_PAGE_PATH} element={<Planets />} />
+        <Route path={CHARACTERS_PAGE_PATH} element={<Characters />} />
+      </Routes>
       <div>Контент</div>
     </Box>
   )
--- src/constants/paths.js
+++ src/constants/paths.js
@@ -1,3 +1,6 @@
-export const CARD_DETAILS_PAGE_PATH = '/card-details'
+export const PLANETS_API_PATH = '/planets'
+export const CHARACTERS_API_PATH = '/people'
+
 export const PLANETS_PAGE_PATH = '/planets'
-export const CHARACTERS_PAGE_PATH = '/people'
+export const CHARACTERS_PAGE_PATH = '/characters'
+export const CARD_DETAILS_PAGE_PATH = '/card-details'
